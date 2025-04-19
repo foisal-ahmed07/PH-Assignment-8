fc8a337ef76a2999a760ad8355583ac6bc2bf824
Foisal Ahmed <shakhfoisal99@gmail.com>
2025-04-19 20:13:18 +0600

    routes added

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { createBrowserRouter } from 'react-router';
+
+const routes = createBrowserRouter([
+    {
+        path: "/",
+        element: <h1>Home</h1>,
+    }
+])
+
+export default routes;
